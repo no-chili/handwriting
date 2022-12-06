94613682759660c8cf983922449b0678a664d970
no-chili <18875854140@163.com>
2022-12-06 11:16:24 +0800

    all

--- Promise/promise.js
+++ Promise/promise.js
@@ -6,6 +6,10 @@ const MyPromise =function(exector){
   this.onFulfilledCallbacks = []
   this.onRejectedCallbacks = []
   const resolve = (value) => {
+    if (value instanceof MyPromise) {
+        value.then(resolve, reject)
+        return
+    }
     if (this.state === PENDING) {
         this.state = FULFILLED
         this.value = value
@@ -31,8 +35,33 @@ const MyPromise =function(exector){
       if (x === promise2) {
           return reject(new TypeError('Chaining cycle detected for promise #<MyPromise>'))
       }
+      let called = false
       if ((typeof x === 'object' && x !== null) || typeof x === 'function') {
-      } else {
+        let then = x.then
+        try {
+            if (typeof then === 'function') {
+                then.call(
+                    x,
+                    (y) => {
+                        if (called) return
+                        called = true
+                        resolvePromise(promise2, y, resolve, reject)
+                    },
+                    (r) => {
+                        if (called) return
+                        called = true
+                        reject(r)
+                    }
+                )
+            } else {
+                resolve(x)
+            }
+        }  catch (e) {
+            if (called) return
+            called = true
+            reject(e)
+        }
+      }  else {
           resolve(x)
       }
     }
@@ -78,6 +107,58 @@ const MyPromise =function(exector){
       }
     })
     return promise2
+  }
+}
+//工具函数封装
+function isIterable(value) {
+  return value !== null && value !== undefined && typeof value[Symbol.iterator] === 'function'
+}
+
+function isPromise(x) {
+  if ((typeof x === 'object' && x !== null) || typeof x == 'function') {
+      let then = x.then
+      return typeof then === 'function'
+  }
+  return false
+}
+// 静态方法模拟
+MyPromise.prototype.resolve=(value)=>{
+  return new MyPromise((resolve, reject)=>{
+      resolve(value)
+  })
+}
+MyPromise.prototype.reject=(reason)=>{
+  return new MyPromise((resolve, reject)=>{
+    reject(reason)
+  })
+}
+MyPromise.prototype.all=(promiseArr)=>{
+  // 返回一个新的promise，内部建一个数组存放已解决promise，当长度等于promiseArr后resolve出去
+  let resArr = [],
+      idx = 0
+  if (!isIterable(promiseArr)) {
+      let type = typeof promiseArr
+      throw TypeError(`${type} is not a iterable (cannot read property Symbol(Symbol.iterator))
+at Function.all (<anonymous>)`)
+  }
+  return new MyPromise((resolve, reject) => {
+      promiseArr.map((promise, index) => {
+          if (isPromise(promise)) {
+              promise.then((res) => {
+                  formatArr(res, index, resolve)
+              }, reject)
+          } else {
+              formatArr(promise, index, resolve)
+          }
+      })
+  })
+
+  function formatArr(value, index, resolve) {
+      resArr[index] = value
+      // if(resArr.length ===promiseArr.length) 在某些时刻不正确，比如数组最后一项先执行完 数组就为[empty,empty,value]
+      if (++idx === promiseArr.length) {
+          resolve(resArr)
+      }
   }
 }
 export {MyPromise}
