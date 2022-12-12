5a9e0c1db26e37b0d953cad7feac75a222bccdb9
no-chili <18875854140@163.com>
2022-12-12 17:02:49 +0800

    audio

---
+++ Array/flat.js
@@ -0,0 +1,14 @@
+export default function flat(arr){
+    let flatArr=[]
+    arr.map(item=>{
+      if(item instanceof Array){
+        flatArr.push(...flat(item))
+      }else{
+        flatArr.push(item) 
+      }
+    })
+    return flatArr
+}
+const arr=['1',[null,'3',[undefined]]]
+const f = flat(arr)
+console.log(f);
